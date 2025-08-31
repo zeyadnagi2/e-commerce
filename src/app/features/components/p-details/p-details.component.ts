9b7179854dd3ef2fdf0f6fd2f974192a488d10cc
Ziad El-Khashab <93553215+zeyadnagi2@users.noreply.github.com>
2025-08-31 23:32:51 +0300

    p-details

--- src/app/features/components/p-details/p-details.component.ts
+++ src/app/features/components/p-details/p-details.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../../shared/services/Products/products.service';
+import { IProduct } from '../../../core/interfaces/iproduct.interface';
 
 @Component({
   selector: 'app-p-details',
   imports: [],
   templateUrl: './p-details.component.html',
-  styleUrl: './p-details.component.css'
+  styleUrl: './p-details.component.css',
 })
-export class PDetailsComponent {
+export class PDetailsComponent implements OnInit {
+  private readonly _ActivatedRoute = inject(ActivatedRoute);
+  private readonly _ProductsService = inject(ProductsService);
 
+  productDetails:IProduct = {} as IProduct;
+  productId!: string;
+
+  ngOnInit(): void {
+    this._ActivatedRoute.paramMap.subscribe({
+      next: (param) => {
+        this.productId = param.get('p_id')!;
+      },
+    });
+
+    this._ProductsService.getSpacificProduct(this.productId).subscribe({
+      next: (res) => {
+        this.productDetails = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
--- src/app/shared/services/Products/products.service.ts
+++ src/app/shared/services/Products/products.service.ts
@@ -4,14 +4,16 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
   private readonly _HttpClient = inject(HttpClient);
 
-  getAllProducts():Observable<any>{
-    return this._HttpClient.get(`${environment.baseUrl}/api/v1/products`)
+  getAllProducts(): Observable<any> {
+    return this._HttpClient.get(`${environment.baseUrl}/api/v1/products`);
   }
-  
+
+  getSpacificProduct(p_id: string): Observable<any> {
+    return this._HttpClient.get(`${environment.baseUrl}/api/v1/products/${p_id}`);
+  }
 }
