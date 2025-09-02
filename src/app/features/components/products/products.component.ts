9b6c75b070adf636c341e2e37c90b2a22331145c
Ziad El-Khashab <93553215+zeyadnagi2@users.noreply.github.com>
2025-09-02 12:25:36 +0300

    done to start again

--- src/app/features/components/products/products.component.ts
+++ src/app/features/components/products/products.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../../shared/services/Products/products.service';
+import { IProduct } from '../../../core/interfaces/iproduct.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  constructor(private _ProductsService: ProductsService) {}
 
+  products!: IProduct[];
+
+  ngOnInit(): void {
+    this._ProductsService.getAllProducts().subscribe({
+      next: (res) => {
+        this.products = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
