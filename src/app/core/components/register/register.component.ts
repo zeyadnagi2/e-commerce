1850cfc210b36b9ac7d8abdf1421162a952989c5
Ziad El-Khashab <93553215+zeyadnagi2@users.noreply.github.com>
2025-09-04 01:11:42 +0300

    register ui

--- src/app/core/components/register/register.component.ts
+++ src/app/core/components/register/register.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { log } from 'console';
+import { register } from 'module';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl(null),
+    email: new FormControl(null),
+    password: new FormControl(null),
+    rePassword: new FormControl(null),
+    phone: new FormControl(null),
+  });
 
+  register(): void {}
 }
