7d0334bdbf6daea45071ab3d2e930520a9a822ec
Ziad El-Khashab <93553215+zeyadnagi2@users.noreply.github.com>
2025-09-04 14:34:41 +0300

    login ui

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { log } from 'console';
+import { AuthService } from '../../../authentication/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private readonly _AuthService = inject(AuthService);
+  private readonly _Router = inject(Router);
 
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,10}$/)]),
+  });
+
+  register(): void {
+    
+  }
 }
