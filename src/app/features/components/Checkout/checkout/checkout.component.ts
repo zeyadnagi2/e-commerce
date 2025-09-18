2518b4f107c2ac906bfe04da468944cfdf6cb910
Ziad El-Khashab <93553215+zeyadnagi2@users.noreply.github.com>
2025-09-18 12:49:36 +0300

    payment done

--- src/app/features/components/Checkout/checkout/checkout.component.ts
+++ src/app/features/components/Checkout/checkout/checkout.component.ts
@@ -1,16 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { OrdersService } from '../../../../shared/services/Orders/orders.service';
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
 })
 export class CheckoutComponent implements OnInit {
   private readonly _ActivatedRoute = inject(ActivatedRoute);
+  private readonly _OrdersService = inject(OrdersService);
 
   cartId!: string;
+
+  checkoutFrom: FormGroup = new FormGroup({
+    details: new FormControl(null, Validators.required),
+    phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]),
+    city: new FormControl(null, Validators.required),
+  });
 
   ngOnInit(): void {
     this._ActivatedRoute.paramMap.subscribe({
@@ -19,5 +27,16 @@ export class CheckoutComponent implements OnInit {
         console.log(this.cartId);
       },
     });
+  }
+
+  checkout() {
+    if (this.checkoutFrom.valid) {
+      this._OrdersService.CheckOutSession(this.cartId, this.checkoutFrom.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          window.open(res.session.url, '_self');
+        },
+      });
+    }
   }
 }
