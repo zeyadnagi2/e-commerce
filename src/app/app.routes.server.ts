19648f693ba77153ac6a5bb62df06c9d2804a5e2
Ziad El-Khashab <93553215+zeyadnagi2@users.noreply.github.com>
2025-09-18 15:18:10 +0300

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   // Dynamic product details → skip prerender
   { path: 'p-details/:p_id', renderMode: RenderMode.Server }, // or RenderMode.Client
+  { path: 'checkout/:c_id', renderMode: RenderMode.Server }, // or RenderMode.Client
 
   // Fallback → prerender everything else
   { path: '**', renderMode: RenderMode.Prerender },
